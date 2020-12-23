dc64066bb33f27f80e6d512015bd2dda3f373b6b
Othon <othon.rdss@gmail.com>
2020-12-23 12:58:07 -0300

    New updates do Heroku deploy

--- the_base/settings.py
+++ the_base/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '89pr!nge%(&sxu_(duf+m3owjs^&nhwjwd=v0(97l3lj37t$m8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['base-abeia.herokuapp.com','127.0.0.1'] 
 
 
 # Application definition
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'the_base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo_1',
+        'USER': 'othonrds',
+        'PASSWORD': '88116566othon',
+        'HOST': 'database-1.ceyrv9od3jjl.sa-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -117,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
@@ -127,3 +135,22 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+#SMTP Configuration
+
+
+
+
+
+
+
+
+
+AWS_ACCESS_KEY_ID = 'AKIAZ2RI6FYJYF54CAJB'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'othonrds-abeia-bucket'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' #look to bucket first!
